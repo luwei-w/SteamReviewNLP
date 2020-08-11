09c4d941a88898865f933bc451172e1b7ae34cac
Luwei Wang <55057094+l563wang@users.noreply.github.com>
2020-08-11 15:23:17 -0400

    Add files via upload

---
+++ code/result.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pre = pd.read_csv("../dataset/svm.csv")
+pred = pre["prediction"].to_numpy()
+
+info = pd.read_csv("../dataset/cleansteam.csv")
+value = info["weighted_vote_score"].to_numpy()
+
+fig = plt.figure()
+plt.scatter(pred, value)
+plt.xticks(np.arange(0,5,step = 1))
+plt.xlabel('Sentiment value')
+plt.ylabel('Weighted_vote_score')
+plt.show()
